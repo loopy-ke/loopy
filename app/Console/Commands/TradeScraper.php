704e6adf04f953f9a9f5986a95c65228d3ac3cf1
Griffins Oringo <oringogriffins78@gmail.com>
2019-09-17 17:55:53 +0300

    forex update

--- app/Console/Commands/TradeScraper.php
+++ app/Console/Commands/TradeScraper.php
@@ -26,39 +26,49 @@ class TradeScraper extends Command
      */
     protected $description = 'Scrap data from meta trader statements';
 
-    protected $url = "https://www.forexfactory.com/explorerapi.php?content=tradeslist&id=172985";
+    protected $urls = [
+        "https://www.forexfactory.com/explorerapi.php?content=tradeslist&id=172985",
+        "https://www.forexfactory.com/explorerapi.php?content=tradeslist&id=176732",
+        "https://www.forexfactory.com/explorerapi.php?content=tradeslist&id=173443",
+        "https://www.forexfactory.com/explorerapi.php?content=tradeslist&id=171470",
+        "https://www.forexfactory.com/explorerapi.php?content=tradeslist&id=156879",
+        "https://www.forexfactory.com/explorerapi.php?content=tradeslist&id=147293",
+        "https://www.forexfactory.com/explorerapi.php?content=tradeslist&id=176270"
+    ];
 
     public function handle()
     {
         $telegram = new Telegram("886550947:AAHhiMlgboPotj_RSVK0pXZ8DRb9DF2OuRA", "TraidtsBot");
         $telegram->enableExternalMySql(\DB::connection()->getPdo());
         $telegram->handleGetUpdates();
-        $key = 'traidts_data';
-        if (!cache()->has($key)) {
-            cache()->put($key, (new Client())->get($this->url)->getBody()->getContents(), Carbon::now()->addMinutes(25));
-        }
-        $data = json_decode(cache($key))->html;
-        ini_set('memory_limit', '1024M');
-        $dom = new Dom;
-        $dom->load($data);
-        $rows = $dom->find('.orders_open > table.explorer_tradeslist__table > tbody > tr.explorer_tradeslist__row');
-        foreach ($rows as $row) {
-            $trade = $row->find('td');
-            $pair = $trade[0]->text;
-            $entry = trim($trade[2]->text);
-            $ticket = $row->getAttribute('data-ticket');
-            if (!cache()->has($ticket)) {
-                $results = Request::sendToActiveChats(
-                    'sendMessage',
-                    ['text' => "$pair\n$entry"],
-                    [
-                        'groups' => true,
-                        'supergroups' => true,
-                        'channels' => false,
-                        'users' => true,
-                    ]
-                );
-                cache()->put($ticket, $row, now()->addMonths(3));
+        foreach ($this->urls as $url) {
+            $key = 'traidts_data' . $url;
+            if (!cache()->has($key)) {
+                cache()->put($key, (new Client())->get($this->url)->getBody()->getContents(), Carbon::now()->addMinutes(25));
+            }
+            $data = json_decode(cache($key))->html;
+            ini_set('memory_limit', '1024M');
+            $dom = new Dom;
+            $dom->load($data);
+            $rows = $dom->find('.orders_open > table.explorer_tradeslist__table > tbody > tr.explorer_tradeslist__row');
+            foreach ($rows as $row) {
+                $trade = $row->find('td');
+                $pair = $trade[0]->text;
+                $entry = trim($trade[2]->text);
+                $ticket = $row->getAttribute('data-ticket');
+                if (!cache()->has($ticket)) {
+                    $results = Request::sendToActiveChats(
+                        'sendMessage',
+                        ['text' => "$pair\n$entry"],
+                        [
+                            'groups' => true,
+                            'supergroups' => true,
+                            'channels' => false,
+                            'users' => true,
+                        ]
+                    );
+                    cache()->put($ticket, $row, now()->addMonths(3));
+                }
             }
         }
     }
